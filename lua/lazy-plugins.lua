b841b11a6607266e5c9ece4801ea465a521b4b3a
Samuel Byland <samuel@byland.name>
2025-06-04 18:58:56 +0200

    change lazy setup

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -59,6 +59,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  --
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
